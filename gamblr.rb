23a413cc752d6e65ea53fad5fbf14f7070234d55
Dan S Turner <turner@nowwithmoredan.com>
2013-12-02 15:16:30 -0800

    Add play

--- gamblr.rb
+++ gamblr.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'data_mapper'
 require 'sinatra/reloader' if development?
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/bet.db")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/gamblr.db")
 
 SITE_TITLE = "Gamblr"
 SITE_DESCRIPTION = "Money is icky!"
@@ -12,6 +12,7 @@ class Gambling
   property :id,           Serial
   property :stake,        Decimal, :required => true
   property :odds,         Decimal, :required => true
+  property :wager,        Decimal, :required => true, :default => 0
   property :lost_game,    Boolean, :required => true, :default => false
   property :started,      DateTime
   property :last_played,  DateTime
@@ -31,5 +32,24 @@ post '/' do
   g.odds  = params[:odds]
   g.started     = Time.now
   g.last_played = Time.now
+  g.save
+  redirect '/'
+end
+
+get '/:id' do
+  @game = Gambling.get params[:id]
+  @title = "Playtime"
+  erb :play
+end
+
+put '/:id' do
+  g = Gambling.get params[:id]
+  g.wager = params[:wager]
+  # g.stake = params[:stake].to_f
+  pot = g.wager * g.odds
+  roll = rand(g.odds)
+  roll == 0 ? g.stake += pot : g.stake -= pot
+  g.last_played = Time.now
+  g.save
   redirect '/'
 end
